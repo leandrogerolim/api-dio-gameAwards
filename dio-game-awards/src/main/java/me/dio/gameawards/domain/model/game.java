b5ab74effa3e2320c9284ae1219e11b549f0f79b
Leandro <leandrogerolim@gmail.com>
2022-12-07 19:01:26 -0400

    classe game criada e configurada.

---
+++ dio-game-awards/src/main/java/me/dio/gameawards/domain/model/game.java
@@ -0,0 +1,59 @@
+package me.dio.gameawards.domain.model;
+
+import jakarta.persistence.*;
+
+
+@Entity(name = "games")
+
+public class game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String name;
+    @Column(length = 500)
+    private String description;
+    private String cover;
+    private long votes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public long getVotes() {
+        return votes;
+    }
+
+    public void setVotes(long votes) {
+        this.votes = votes;
+    }
+}
