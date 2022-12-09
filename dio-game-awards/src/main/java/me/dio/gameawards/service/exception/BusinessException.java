27aa6fda5158c604da4e4817ed353d1fae9fba30
Leandro <leandrogerolim@gmail.com>
2022-12-09 15:33:59 -0400

    corrigido BusinessException

--- dio-game-awards/src/main/java/me/dio/gameawards/service/exception/BusinessException.java
+++ dio-game-awards/src/main/java/me/dio/gameawards/service/exception/BusinessException.java
@@ -3,6 +3,7 @@ package me.dio.gameawards.service.exception;
 public class BusinessException extends RuntimeException{
     private static final long serialVersionUID = 1L;
 
+
     public BusinessException(String message){
         super(message);
     }
