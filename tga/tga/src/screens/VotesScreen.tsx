ed8cc43c4af719a9e7985d2a03fb668e7bc99a9f
Leandro <leandrogerolim@gmail.com>
2022-12-08 23:20:31 -0400

    Realizado configuração e testes no android auto

---
+++ tga/tga/src/screens/VotesScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export function VotesScreen(){
+    return (
+        <View>
+            <Text>Exodia Obliterar</Text>
+        </View>
+    )
+}
