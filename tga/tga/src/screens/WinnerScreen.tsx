d07bce8a79a85718804dd8dc3b46c55b73593f13
Leandro <leandrogerolim@gmail.com>
2022-12-10 13:31:03 -0400

    tela Winner criada e configurada.

---
+++ tga/tga/src/screens/WinnerScreen.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text, StyleSheet, ImageBackground } from 'react-native';
+
+export function WinnerScreen (){
+    return(
+    <View style={styles.container}>
+        <Text>Winner Works!!!!</Text>
+    </View>
+    )
+}
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        color:'#fff',
+        backgroundColor:'#191919',
+        alignItems:'center',
+        justifyContent:'center'
+    }
+})
