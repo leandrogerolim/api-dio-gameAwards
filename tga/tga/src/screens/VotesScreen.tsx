25f6be25e210297185d3e6f572a1aa0920f06e11
Leandro <leandrogerolim@gmail.com>
2022-12-10 13:19:44 -0400

    criado a const styles.

--- tga/tga/src/screens/VotesScreen.tsx
+++ tga/tga/src/screens/VotesScreen.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import {View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet, ImageBackground } from 'react-native';
 
 export function VotesScreen(){
     return (
-        <View>
-
+        <View style={styles.container}>
             <Text>
-                <StyleSheet>Exodia Obliterar é ç ã teste</StyleSheet>
-                
+                Exodia Obliterar é ç ã teste          
             </Text>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    container:{
+        flex: 1
+        backgroudColor:'#191919'
+    }
+})
