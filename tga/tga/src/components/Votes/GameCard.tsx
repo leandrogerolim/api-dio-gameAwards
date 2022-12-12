2213efd2e28e432f3edcf51e706498650bd12459
Leandro <leandrogerolim@gmail.com>
2022-12-11 20:16:15 -0400

    Votes e game card criado e importado a estilização

---
+++ tga/tga/src/components/Votes/GameCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native'
+
+export function GameCard(){
+ return(
+    <View>
+
+    </View>
+ )
+}
+
+const style = StyleSheet.create({
+
+})
