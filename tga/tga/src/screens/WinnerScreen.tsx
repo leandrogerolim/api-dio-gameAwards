57d803294d06d04138e774ec4de4a5eb06d772ae
Leandro <leandrogerolim@gmail.com>
2022-12-11 23:27:22 -0400

    tela Winner  mostrando o resultado correto

--- tga/tga/src/screens/WinnerScreen.tsx
+++ tga/tga/src/screens/WinnerScreen.tsx
@@ -1,20 +1,54 @@
-import React from 'react';
-import {View, Text, StyleSheet, ImageBackground } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import { View, Text, StyleSheet } from 'react-native';
+import { gameInterface } from '../interfaces/gameInterface';
+import { clientGetWinner } from '../api/api';
+import { Winner } from '../components/Winner/Winner';
 
-export function WinnerScreen (){
-    return(
-    <View style={styles.container}>
-        <Text>Winner Works!!</Text>
-    </View>
-    )
+interface gameProps{
+  id:0,
+  name:"--",
+  description:"--",
+  cover:"",
+  votes: 0,
 }
-const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        color:'#fff',
-        backgroundColor:'#191919',
-        alignItems:'center',
-        justifyContent:'center'
-    }
-})
 
+export function WinnerScreen(){
+
+  const [game, setGame] = useState<gameProps>({
+    id:0,
+    name:"--",
+    description:"--",
+    cover:"",
+    votes: 0,
+  });
+
+  useFocusEffect(
+    useCallback(() =>{
+      (async () => {
+        const winner = await clientGetWinner()
+        setGame(winner)
+      })()
+    },[])
+  )
+
+  return(
+    <View style={styles.container}>
+      <Winner 
+        name={game.name}
+        cover={game.cover}
+        votes={game.votes}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color: '#fff',
+    backgroundColor: '#191919',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
