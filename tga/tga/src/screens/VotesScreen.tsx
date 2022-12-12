9ffebd8817afbdd13211d4bad32e59c1047e7502
Leandro <leandrogerolim@gmail.com>
2022-12-11 22:36:40 -0400

    configuração para reconhecer png.

--- tga/tga/src/screens/VotesScreen.tsx
+++ tga/tga/src/screens/VotesScreen.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import {View, Text, StyleSheet, ImageBackground } from 'react-native';
+import {View, Text, StyleSheet, ImageBackground, ScrollView } from 'react-native';
+import {GameCard} from '../components/Votes/GameCard'
+import { Header } from '../components/Votes/Header';
+
 
 export function VotesScreen(){
     return (
         <View style={styles.container}>
-            <Text>
-                Exodia Obliterar teste          
-            </Text>
+            <Header/>
+            <ScrollView>
+                
+            </ScrollView>
         </View>
     )
 }
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        color:'#fff',
-        backgroundColor:'#191919',
-        alignItems:'center',
-        justifyContent:'center'
+    container: {
+      flex: 1,
+      color:'#fff',
+      backgroundColor: '#191919',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    gameArea:{
+      flex:1,
+      paddingTop:10,
+      paddingBottom: 20,
+      width:'100%'
     }
-})
+  });
