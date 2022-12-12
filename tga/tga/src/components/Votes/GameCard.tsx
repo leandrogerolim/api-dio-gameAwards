60f36b97ff971ae6b854de178147877516b76f39
Leandro <leandrogerolim@gmail.com>
2022-12-11 20:35:59 -0400

    game card e styleSheet configurado

--- tga/tga/src/components/Votes/GameCard.tsx
+++ tga/tga/src/components/Votes/GameCard.tsx
@@ -1,14 +1,48 @@
 import React from "react";
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Image} from 'react-native'
 
 export function GameCard(){
  return(
-    <View>
+    <View style={style.Cardcontainer}>
+        <View>
+        <Image
+            source = {{uri:''}}
+            style = {style.card}
+        />
+        
+        </View>
 
     </View>
  )
 }
 
 const style = StyleSheet.create({
+    Cardcontainer:{
+        borderWidth:2,
+        padding:10,
+        borderBottomColor:'#fff',
+        width:'100%',
+        borderRadius:8,
+        flexDirection: 'row',
+        backgroundColor:'#404040'
+      },
+      card:{
+        borderRadius:10,
+        width: 200, 
+        height: 300,
+      },
+      label:{
+        fontSize:22,
+        color:'white',
+        paddingBottom:15,
+        textAlign:'center',
+      },
+      infoContainer:{
+        flex:1,
+        justifyContent:'flex-start',
+        alignItems:'center',
+        textAlign:'center',
+        paddingTop:20,
+      }
+    })
 
-})
