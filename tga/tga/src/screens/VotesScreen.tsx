b3769e42b3a8a97ac25e450d5b96498b18677c85
Leandro <leandrogerolim@gmail.com>
2022-12-11 23:15:55 -0400

    style gameArea ok.

--- tga/tga/src/screens/VotesScreen.tsx
+++ tga/tga/src/screens/VotesScreen.tsx
@@ -17,7 +17,7 @@ export function VotesScreen(){
     return (
         <View style={styles.container}>
             <Header/>
-            <ScrollView>
+            <ScrollView style={styles.gameArea}>
                 {gameList.map(game => GameCard(game))}
             </ScrollView>
         </View>
